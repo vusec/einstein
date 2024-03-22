e5c9e0a4416e0f4a76e26b98d4e6001eb1cfbaba
Brian Johannesmeyer <bjohannesmeyer@gmail.com>
2024-03-22 16:23:38 +0100

    apps: Improve stop-all-servers. Run it after app-eval-brief.

--- apps/stop-all-servers.sh
+++ apps/stop-all-servers.sh
@@ -1,6 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+set +e
 
+echo "Stopping all servers..."
+
+# Run './serverctl stop' for all apps
 find ./*/serverctl | grep -v "^\./tests\/" | sed 's/\/serverctl.*//' | xargs -I{} sh -c "cd {} && ./serverctl stop" > /dev/null
 
+# Kill scripts
+pkill clientctl
+pkill runbench
+pkill serverctl
+
+# Kill processes using port 1080, just to be safe
+KILL_PID=$(sudo lsof -t -i:1080)
+[[ ! -z ${KILL_PID} ]] && kill ${KILL_PID}
+
+exit 0
